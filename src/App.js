d2d2359da8f0f5eab1450276bff5c21d40067f95
Jamal Ahmed <j8ahmed@gmail.com>
2021-02-07 19:11:23 -0500

    removed unnecessary project files.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import J8ahmed from './projects/j8ahmed_portfolio'
+import RestaurantMenuApp from './restaurant_menu'
 
 const App = () => {
     return (
     <React.Fragment> 
-        <J8ahmed />
+        <RestaurantMenuApp />
     </React.Fragment>)
 }
 
